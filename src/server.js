95b9fb54c179eb54461b1df5d574bf0205da9fdb
Mark Smith <mark.smith@purple.telstra.com>
2022-04-21 10:59:58 +1000

    initial version

---
+++ src/server.js
@@ -0,0 +1,94 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
+import Config from "./config";
+import path from "path";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/build")));
+app.use(bodyParser.json());
+
+app.get("/api/articles/:name/", async (req, res) => {
+  withDb(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
+});
+
+app.post("/api/articles/:name/upvote", async (req, res) => {
+  withDb(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvotes: articleInfo.upvotes + 1,
+        },
+      }
+    );
+
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  withDb(async (db) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+app.listen(8000, () => {
+  console.log("listening on 8000");
+});
+
+const withDb = async (operations, res) => {
+  const client = new MongoClient(Config.mongoDbConnectionString);
+
+  try {
+    await client.connect();
+    const db = client.db("my-blog");
+    await operations(db);
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  } finally {
+    await client.close();
+  }
+};
